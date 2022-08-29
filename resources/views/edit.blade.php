b3436ce4ab023f57899475143a8cf853865b7ce4
laukpindang <ferdiansyahbandung3@gmail.com>
2022-08-29 21:04:51 +0700

    Mengubah beberapa judul

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+	<title>Pegawai</title>
 </head>
 <body>
  
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Edit Pegawai</h3>
+	<h1>Edit Pegawai</h1>
  
 	<a href="/pegawai"> Kembali</a>
 	
